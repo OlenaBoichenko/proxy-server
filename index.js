52913ac27e11e89e7fc89ea120c86e8c13eb9cca
OlenaBoichenko <ol.boichenko.dev@gmail.com>
2024-09-05 21:43:18 -0400

    updated cors

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ import cors from 'cors';
 import fetch from 'node-fetch';
 
 const app = express();
+app.use(cors({
+    origin: '*',
+  }));
 
-app.use(cors());
-
-// Прокси-эндпоинт для работы с API
 app.get('/products', async (req, res) => {
   try {
     const response = await fetch('https://fakestoreapi.com/products');
+    if (!response.ok) {
+      console.error(`Ошибка при запросе к API: ${response.statusText}`);
+      return res.status(response.status).json({ error: 'Ошибка при запросе к API' });
+    }
     const data = await response.json();
     res.json(data);
   } catch (error) {
+    console.error('Ошибка сервера:', error.message);
     res.status(500).json({ error: 'Ошибка сервера' });
   }
 });
@@ -21,4 +26,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
+
 
