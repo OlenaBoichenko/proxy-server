dab0449801d1fda9926a7e98f00fb9820f087f8b
OlenaBoichenko <ol.boichenko.dev@gmail.com>
2024-09-06 12:18:12 -0400

    changed settings

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(cors({
     origin: '*',
   }));
 
+  const API_KEY = process.env.API_KEY;
+
 app.get('/products', async (req, res) => {
   try {
-    const response = await fetch('https://fakestoreapi.com/products');
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=12&has_breeds=true', {
+        headers: { 'Authorization': `Bearer ${API_KEY}` }
+    });
     if (!response.ok) {
       console.error(`Ошибка при запросе к API: ${response.statusText}`);
       return res.status(response.status).json({ error: 'Ошибка при запросе к API' });
